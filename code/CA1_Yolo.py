940bd0514eac42ed779af68c9fddec4d4148dff3
Raymond <35454616+raymondng76@users.noreply.github.com>
2019-10-13 12:52:28 +0800

    add train valid split

--- code/CA1_Yolo.py
+++ code/CA1_Yolo.py
@@ -37,19 +37,75 @@ class YoloV3():
         '''Ctor'''
         # super().__init__(*args, **kwargs):
         pass
-    def fit(self, img_dir, annotation_dir):
+    def fit(self, img_dir, annotation_dir, train_size):
         '''Fit without augmentation'''
         all_anno, labels, anchor_boxes = self._process_dataset(img_dir, annotation_dir)
-    def fit_generator(self, img_dir, annotation_dir):
-        '''Fit with augmentationo'''
+        train_anno, valid_anno = _train_valid_split(all_anno)
+    def fit_generator(self, img_dir, annotation_dir, train_size):
+        '''Fit with augmentation'''
         all_anno, labels, anchor_boxes = self._process_dataset(img_dir, annotation_dir)
+        train_anno, valid_anno = _train_valid_split(all_anno)
     def predict(self):
         pass
     def _process_dataset(self, img_dir, annotation_dir):
         all_anno, labels = read_annotation_files(img_dir, annotation_dir)
         anchor_boxes = generateAnchorBoxes(all_anno, labels)
         return all_anno, labels, anchor_boxes
+    def _train_valid_split(self, all_anno, train_size=0.8):
+        train_valid_split = int(train_size * len(all_anno))
+        np.random.shuffle(all_anno)
+        train_anno = all_anno[:train_valid_split]
+        valid_anno = all_anno[train_valid_split:]
+        return train_anno, valid_anno
 #------------------------------------
+#%%
+#----------VOC Parser----------
+def read_annotation_files(image_dir, annnotation_dir):
+    all_annotations = []
+    labels = []
+    for anno in sorted(os.listdir(annnotation_dir)):
+        img = {'object':[]}
+        try:
+            xml_tree = ET.parse(annnotation_dir + anno)
+        except Exception as ex:
+            print(ex)
+            print(f'Error parsing annotation xml: {annnotation_dir + anno}')
+            continue
+        for elem in xml_tree.iter():
+            if 'filename' in elem.tag:
+                img['filename'] = image_dir + elem.text
+            if 'width' in elem.tag:
+                img['width'] = int(elem.text)
+            if 'height' in elem.tag:
+                img['height'] = int(elem.text)
+            if 'object' in elem.tag:
+                obj = {}
+                for attribute in list(elem):
+                    if 'name' in attribute.tag:
+                        obj['name'] = attribute.text
+                        if obj['name'] in labels:
+                            labels[obj['name']] += 1
+                        else:
+                            labels[obj['name']] = 1
+                        img['object'] += [obj]
+                    if 'bndbox' in attribute.tag:
+                        for dim in list(attribute):
+                            if 'xmin' in dim.tag:
+                                obj['xmin'] = int(round(float(dim.text)))
+                            if 'ymin' in dim.tag:
+                                obj['ymin'] = int(round(float(dim.text)))
+                            if 'xmax' in dim.tag:
+                                obj['xmax'] = int(round(float(dim.text)))
+                            if 'ymax' in dim.tag:
+                                obj['ymax'] = int(round(float(dim.text)))
+        if len(img['object']) > 0:
+            bb_list = []
+            for obj in img['object']:
+                bb_list.append(ia.BoundingBox(x1=obj['xmin'], y1=obj['ymin'], x2=obj['xmax'], y2=obj['ymax'], label=obj['name']))
+            img['bbs'] = ia.BoundingBoxesOnImage(bb_list, shape=(img['width'], img['height']))
+            all_annotations += [img]
+    return all_annotations, labels
+#------------------------------
 #%%
 #----------Generate Anchor boxes----------
 def calculate_iou(anno, centroids):
@@ -161,131 +217,84 @@ class DataGenerator(Sequence):
         index = self.index[index * self.batch_size : (index + 1) * self.batch_size]
         labels = np.array
 #----------------------------------------------
-#%%
-#----------VOC Parser----------
-def read_annotation_files(image_dir, annnotation_dir):
-    all_annotations = []
-    labels = []
-    for anno in sorted(os.listdir(annnotation_dir)):
-        img = {'object':[]}
-        try:
-            xml_tree = ET.parse(annnotation_dir + anno)
-        except Exception as ex:
-            print(ex)
-            print(f'Error parsing annotation xml: {annnotation_dir + anno}')
-            continue
-        for elem in xml_tree.iter():
-            if 'filename' in elem.tag:
-                img['filename'] = image_dir + elem.text
-            if 'width' in elem.tag:
-                img['width'] = int(elem.text)
-            if 'height' in elem.tag:
-                img['height'] = int(elem.text)
-            if 'object' in elem.tag:
-                obj = {}
-                for attribute in list(elem):
-                    if 'name' in attribute.tag:
-                        obj['name'] = attribute.text
-                        if obj['name'] in labels:
-                            labels[obj['name']] += 1
-                        else:
-                            labels[obj['name']] = 1
-                        img['object'] += [obj]
-                    if 'bndbox' in attribute.tag:
-                        for dim in list(attribute):
-                            if 'xmin' in dim.tag:
-                                obj['xmin'] = int(round(float(dim.text)))
-                            if 'ymin' in dim.tag:
-                                obj['ymin'] = int(round(float(dim.text)))
-                            if 'xmax' in dim.tag:
-                                obj['xmax'] = int(round(float(dim.text)))
-                            if 'ymax' in dim.tag:
-                                obj['ymax'] = int(round(float(dim.text)))
-        if len(img['object']) > 0:
-            bb_list = []
-            for obj in img['object']:
-                bb_list.append(ia.BoundingBox(x1=obj['xmin'], y1=obj['ymin'], x2=obj['xmax'], y2=obj['ymax'], label=obj['name']))
-            img['bbs'] = ia.BoundingBoxesOnImage(bb_list, shape=(img['width'], img['height']))
-            all_annotations += [img]
-    return all_annotations, labels
-#------------------------------
+
 #%%
 #---------- Class for Bounding Box + util functions ----------
-class BoundingBox:
-    '''Bounding Box definition'''
-    def __init__(self, xmin, ymin, xmax, ymax, c = None, classes = None):
-        self.xmin=xmin
-        self.ymin=ymin
-        self.xmax=xmax
-        self.ymax=ymax
-        self.c=c
-        self.classes=classes
-        self.label = None
-        self.score = None
-    def get_label(self):
-        if self.label == None:
-            self.label = np.argmax(self.classes)
-        return self.label
-    def get_score(self):
-        if self.score == None:
-            self.score = self.classes[self.get_label()]
-        return self.score
+# class BoundingBox:
+#     '''Bounding Box definition'''
+#     def __init__(self, xmin, ymin, xmax, ymax, c = None, classes = None):
+#         self.xmin=xmin
+#         self.ymin=ymin
+#         self.xmax=xmax
+#         self.ymax=ymax
+#         self.c=c
+#         self.classes=classes
+#         self.label = None
+#         self.score = None
+#     def get_label(self):
+#         if self.label == None:
+#             self.label = np.argmax(self.classes)
+#         return self.label
+#     def get_score(self):
+#         if self.score == None:
+#             self.score = self.classes[self.get_label()]
+#         return self.score
 
-def getBoundBoxColor(label):
-    colors = [
-        [255, 0, 0],
-        [0, 255, 0],
-        [0, 0, 255]
-    ]
-    if label < len(colors):
-        return colors[label]
-    else:
-        return colors[0] # return default
+# def getBoundBoxColor(label):
+#     colors = [
+#         [255, 0, 0],
+#         [0, 255, 0],
+#         [0, 0, 255]
+#     ]
+#     if label < len(colors):
+#         return colors[label]
+#     else:
+#         return colors[0] # return default
 
-def interval_overlap(interval_a, interval_b):
-    x1, x2 = interval_a
-    x3, x4 = interval_b
+# def interval_overlap(interval_a, interval_b):
+#     x1, x2 = interval_a
+#     x3, x4 = interval_b
     
-    if x3 < x1:
-        if x4 < x1:
-            return 0
-        else:
-            return min(x2, x4) - x1
-    else:
-        if x2 < x3:
-            return 0
-        else:
-            return min(x2, x4) - x3
+#     if x3 < x1:
+#         if x4 < x1:
+#             return 0
+#         else:
+#             return min(x2, x4) - x1
+#     else:
+#         if x2 < x3:
+#             return 0
+#         else:
+#             return min(x2, x4) - x3
 
-def boundbox_iou(bx1, bx2):
-    intsec_w = interval_overlap([bx1.xmin, bx2.xmax], [bx2.xmin, bx2.xmax])
-    intsec_h = interval_overlap([bx1.ymin, bx2.ymax], [bx2.ymin, bx2.ymax])
-    intsec = intsec_w * intsec_h
-    w1, h1 = bx1.xmax-bx1.xmin, bx1.ymax-bx1.ymin
-    w2, h2 = bx2.xmax-bx2.xmin, bx2.ymax-bx2.ymin
-    union = w1*h1 + w2*h2 - intsec
-    return float(intsec)/union
+# def boundbox_iou(bx1, bx2):
+#     intsec_w = interval_overlap([bx1.xmin, bx2.xmax], [bx2.xmin, bx2.xmax])
+#     intsec_h = interval_overlap([bx1.ymin, bx2.ymax], [bx2.ymin, bx2.ymax])
+#     intsec = intsec_w * intsec_h
+#     w1, h1 = bx1.xmax-bx1.xmin, bx1.ymax-bx1.ymin
+#     w2, h2 = bx2.xmax-bx2.xmin, bx2.ymax-bx2.ymin
+#     union = w1*h1 + w2*h2 - intsec
+#     return float(intsec)/union
 
-def draw_boundbox(img, boxes, labels, thresh):
-    for box in boxes:
-        label_str=''
-        label = None
-        for idx in range(len(labels)):
-            if box.classes[idx] > thresh:
-                if label_str != '': label_str +=', '
-                label_str += (labels[idx] + ' ' + str(round(box.get_score() * 100, 2)) + '%')
-                label = idx
-        if label >= 0:
-            text_size = cv2.getTextSize(label_str, cv2.FONT_HERSHEY_SIMPLEX, img.shape[0], 5)
-            width, height = text_size[0][0], text_size[0][1]
-            region = np.array([[box.xmin-3, box.ymin],
-                               [box.xmin-3, box.ymin-height-26],
-                               [box.xmin+width+13, box.ymin-height-26],
-                               [box.xmin+width+13, box.ymin]], dtype='int32')
-            cv2.rectangle(img=img, pt1=(box.xmin, box.ymin), pt2=(box.xmax, box.ymax), color=getBoundBoxColor(label), thickness=4)
-            cv2.fillPoly(img=img, pts=[region], color=getBoundBoxColor(label))
-            cv2.putText(img=img, text=label_str, org=(box.xmin+13, box.ymin-13), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontscale=img.shape[0], color=(0, 0, 0), thickness=2)
-    return img
+# def draw_boundbox(img, boxes, labels, thresh):
+#     for box in boxes:
+#         label_str=''
+#         label = None
+#         for idx in range(len(labels)):
+#             if box.classes[idx] > thresh:
+#                 if label_str != '': label_str +=', '
+#                 label_str += (labels[idx] + ' ' + str(round(box.get_score() * 100, 2)) + '%')
+#                 label = idx
+#         if label >= 0:
+#             text_size = cv2.getTextSize(label_str, cv2.FONT_HERSHEY_SIMPLEX, img.shape[0], 5)
+#             width, height = text_size[0][0], text_size[0][1]
+#             region = np.array([[box.xmin-3, box.ymin],
+#                                [box.xmin-3, box.ymin-height-26],
+#                                [box.xmin+width+13, box.ymin-height-26],
+#                                [box.xmin+width+13, box.ymin]], dtype='int32')
+#             cv2.rectangle(img=img, pt1=(box.xmin, box.ymin), pt2=(box.xmax, box.ymax), color=getBoundBoxColor(label), thickness=4)
+#             cv2.fillPoly(img=img, pts=[region], color=getBoundBoxColor(label))
+#             cv2.putText(img=img, text=label_str, org=(box.xmin+13, box.ymin-13), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontscale=img.shape[0], color=(0, 0, 0), thickness=2)
+#     return img
 #--------------------------------------------
 #----------Loss Layer----------
 # Refering to this https://keras.io/layers/writing-your-own-keras-layers/
